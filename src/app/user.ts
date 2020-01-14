e34f9b17d59002c382eaeedcbb07ac963bdb946d
Klazen <cmurphy1337@live.com>
2020-01-13 22:45:01 -0600

    continue work on user profile

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,13 @@
 export class User {
   id: number;
   name: string;
+  dateCreated: string;
+  twitchLink: string;
+  youtubeLink: string;
+  nicoLink: string;
+  twitterLink: string;
+  bio: string;
+  email: string;
   isAdmin: boolean;
   token: string;
 }
