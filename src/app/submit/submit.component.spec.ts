4ec6901362c5dc47f1b7878e46ff7af6854d2503
Klazen <cmurphy1337@live.com>
2020-01-28 18:34:46 -0600

    submission specs

--- src/app/submit/submit.component.spec.ts
+++ src/app/submit/submit.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmitComponent } from './submit.component';
+import { GameService } from '../game.service';
+import { UserService } from '../user.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SubmitComponent', () => {
   let component: SubmitComponent;
@@ -8,7 +19,24 @@ describe('SubmitComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubmitComponent ]
+      declarations: [ 
+        SubmitComponent
+       ],
+      imports: [
+        RouterTestingModule,
+        MatSnackBarModule,
+        HttpClientTestingModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        GameService,
+        UserService
+      ]
     })
     .compileComponents();
   }));
