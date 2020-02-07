d606db6639d266c246589b9f94f23b57b40e1e63
Klazen <cmurphy1337@live.com>
2020-02-06 23:06:58 -0600

    forgot password specs

--- src/app/forgot-password-dialog/forgot-password-dialog.component.spec.ts
+++ src/app/forgot-password-dialog/forgot-password-dialog.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ForgotPasswordDialogComponent } from './forgot-password-dialog.component';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ForgotPasswordDialogComponent', () => {
   let component: ForgotPasswordDialogComponent;
@@ -8,7 +14,19 @@ describe('ForgotPasswordDialogComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordDialogComponent ]
+      declarations: [ ForgotPasswordDialogComponent ],
+      imports: [ 
+        FormsModule, 
+        MatInputModule, 
+        MatDialogModule, 
+        MatButtonModule,
+        BrowserModule,
+        BrowserAnimationsModule
+      ],
+        providers: [
+          { provide: MatDialogRef, useValue: {} },
+          { provide: MAT_DIALOG_DATA, useValue: [] },
+        ]
     })
     .compileComponents();
   }));
