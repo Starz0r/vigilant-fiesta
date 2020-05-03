eeadd242a3b79b88d56d6bc1b774251a1c969f74
Klazen <cmurphy1337@live.com>
2020-05-03 17:12:54 -0500

    password-reset component specs

--- src/app/password-reset/password-reset.component.spec.ts
+++ src/app/password-reset/password-reset.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PasswordResetComponent } from './password-reset.component';
+import { UserService } from '../user.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('PasswordResetComponent', () => {
   let component: PasswordResetComponent;
@@ -8,7 +17,19 @@ describe('PasswordResetComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PasswordResetComponent ]
+      declarations: [ PasswordResetComponent ],
+      imports: [
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatSnackBarModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [ UserService ]
     })
     .compileComponents();
   }));
