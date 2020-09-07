a714894d8d859074f37d1faf18375d70ce6ee081
Klazen108 <cmurphy1337@live.com>
2020-09-07 00:06:46 -0500

    update imports for test i hate material

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -5,6 +5,12 @@ import { MatTableModule } from '@angular/material/table';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { GameService } from '../game.service';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('UserListComponent', () => {
   let component: UserListComponent;
@@ -14,7 +20,16 @@ describe('UserListComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ UserListComponent ],
       imports: [
-        MatTableModule,RouterTestingModule,HttpClientTestingModule
+        MatTableModule,
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatAutocompleteModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        FormsModule,
+        MatInputModule,
+        NoopAnimationsModule
       ],
       providers: [GameService]
     })
